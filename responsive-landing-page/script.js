2f92b3b8ae7e2efc2827183a86ccc1b82c1d8919
tejasambore <tejasambore2003@gmail.com>
2024-11-20 13:09:13 +0530

    task1 responsive-landing-page | completed

---
+++ responsive-landing-page/script.js
@@ -0,0 +1,11 @@
+// Get the navbar
+const navbar = document.getElementById("navbar");
+
+// Add a scroll event listener
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
